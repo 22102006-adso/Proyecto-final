5dcfa67633533082fca4c10f4b398a7a9e256e6e
22102006-adso <juanfelipemarin222@gmail.com>
2024-12-06 18:55:28 -0500

    Update producto.php

--- app/Models/producto.php
+++ app/Models/producto.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class producto extends Model
+class Producto extends Model
 {
     use HasFactory;
+    protected $table = 'productos';
     protected $fillable = ['nombre', 'precio', 'stock', 'id_categoria', 'id_proveedor'];
 
     public function categoria(){
@@ -15,6 +16,7 @@ class producto extends Model
     }
 
     public function proveedor(){
-        return $this->belongsTo(proveedor::class, 'id_proveedor');
+        return $this->belongsTo(Proveedor::class, 'id_proveedor');
     }
 }
+
