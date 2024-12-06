26fcf4e98ebfdd656e9249ce3e9396b2e5591ba3
22102006-adso <juanfelipemarin222@gmail.com>
2024-12-06 18:56:02 -0500

    Update proveedores.php

--- app/Models/proveedores.php
+++ app/Models/proveedores.php
@@ -5,14 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class proveedor extends Model
+class Proveedor extends Model
 {
     use HasFactory;
     protected $table = 'proveedores';
     protected $fillable = ['nombre', 'direccion', 'telefono', 'email', 'contacto', 'descripion'];
 
     public function productos(){
-        return $this->hasMany(producto::class);
+        
+        return $this->hasMany(Producto::class);
     }
 
 }
+
