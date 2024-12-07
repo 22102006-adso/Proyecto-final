768efc14c026c6afc788744f9d2a2a716a1e1457
22102006-adso <juanfelipemarin222@gmail.com>
2024-12-06 19:04:39 -0500

    Update edit.blade.php

--- resources/views/producto/edit.blade.php
+++ resources/views/producto/edit.blade.php
@@ -1,40 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editar Producto</title>
+    <link rel="stylesheet" href="{{asset('css/estilos.css') }}">
 </head>
+
 <body>
-    <a href="{{ route('producto.index') }}" >Volver a lista</a>
-        <div class="container">
-            <h1>Editar producto</h1>
+    <a href="{{ route('productos.index') }}" class="route">Volver a lista</a>
 
-        <form method="POST" action="{{ route('producto.update', $categorias->id) }}">
-        @csrf
-        @method('PUT')
+    <div class="container">
+        <h1>Editar Producto</h1>
 
-        <div class="form-group">
+        <form method="POST" action="{{ route('productos.update', $producto->id) }}">
+            @csrf
+            @method('PUT')
+
+        
+            <div class="form-group">
                 <label for="nombre">Nombre</label>
-                <input type="text" class="form-control" id="nombre" name="nombre" value="{{ old('nombre', $productos) }}" required>
-            </div> 
+                <input type="text" class="form-control" id="nombre" name="nombre" value="{{ old('nombre', $producto->nombre) }}" required>
+                @error('nombre')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
+            </div>
             <div class="form-group">
                 <label for="precio">Precio</label>
-                <input type="number" class="form-control" id="precio" name="precio" {{ old('precio', $productos) }}>
+                <input type="number" class="form-control" id="precio" name="precio" value="{{ old('precio', $producto->precio) }}" min="0">
+                @error('precio')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="stock">Stock</label>
-                <input type="number" class="form-control" id="stock" name="stock" {{ old('stock', $productos) }}>
+                <input type="number" class="form-control" id="stock" name="stock" value="{{ old('stock', $producto->stock) }}" min="0">
+                @error('stock')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
             </div>
             <div class="form-group">
-                <label for ="id_categoria">Categoria</label>
+                <label for="id_categoria">Categoría</label>
                 <select class="form-control" id="id_categoria" name="id_categoria" required>
                     @foreach($categorias as $categoria)
-                        <option value="{{$categoria->id}}" @if ($producto->id_categoria == $categoria->id) selected>{{$categoria->nombre}}</option>
+                        <option value="{{ $categoria->id }}" 
+                            {{ old('id_categoria', $producto->id_categoria) == $categoria->id ? 'selected' : '' }}>
+                            {{ $categoria->nombre }}
+                        </option>
                     @endforeach
                 </select>
+                @error('id_categoria')
+                    <div style="color: red;">{{ $message }}</div>
+                @enderror
             </div>
-            <button type="submit" class="btn btn-primary">Actualizar</button>
+
+            <button type="submit" class="button">Actualizar</button>
         </form>
+    </div>
 </body>
+
 </html>
